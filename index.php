791ca9dfa27dc32ffab43c628df4d94677e55d6d
xWoozixx <127356493+xWoozixx@users.noreply.github.com>
2023-10-16 15:07:23 +1100

    Create index.php

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>LAMP sur un server NFS</title>
+    <?php echo "Hello, this is a simple PHP test!"; ?>
+</head>
+        <style>
+            body {
+                font-family: Arial, sans-serif;
+                background-color: #f2f2f2;
+                padding: 20px;
+            }
+
+            h1 {
+                color: #333;
+            }
+
+            p {
+                color: #666;
+                line-height: 1.5;
+            }
+            </style>
+<body>
+    <h1>Bienvenue sur l'app1 du Vice-rectorat !</h1>
+    <p>
+        Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis
+        egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet,
+        ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est.
+        Mauris placerat eleifend leo.
+    </p>
+    <form action="connect.php" method="post">
+        <input type="submit" name="submit" value="Exécuter connect.php">
+    </form>
+</body>
+</html>
